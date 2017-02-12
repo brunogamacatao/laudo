1f6eec6fc23213f928053b928dfdb37a1d13ffa1
Bruno Gama Catao <brunogamacatao@gmail.com>
2017-02-12 00:50:53 -0300

    Alterada a url do MongoDB

--- server/config/constantes.js
+++ server/config/constantes.js
@@ -1,6 +1,6 @@
 const constantes = {
   DATABASE_URL_DEV: 'mongodb://localhost/laudo',
-  DATABASE_URL_PROD: 'mongodb://heroku_g8h2pt53:vg37cvp3pdodef206fv59fef94@ds149059.mlab.com:49059/heroku_g8h2pt53',
+  DATABASE_URL_PROD: 'mongodb://heroku_wkb9m7x7:ia454oarn83kq4p4vml4vhk0tf@ds149059.mlab.com:49059/heroku_wkb9m7x7',
   VIEW_ENGINE: 'ejs',
   VIEWS_PATH: '../client/dist',
   STATICS_PATH: '../client/dist',
