cb7ea50ae0bfec98b39035ca8fd6a944fcab9c53
Bruno Gama Catao <brunogamacatao@gmail.com>
2017-06-13 10:34:46 -0300

    Pronto

--- client/src/js/controllers/questionario.js
+++ client/src/js/controllers/questionario.js
@@ -2,11 +2,27 @@ import angular from 'angular';
 
 const controllers = angular.module('ipesq.controllers');
 
-controllers.controller('NovoQuestionarioController', ['$rootScope', '$scope', '$stateParams',
-  function($rootScope, $scope, $stateParams) {
+controllers.controller('NovoQuestionarioController', ['$rootScope', '$scope', '$state', '$stateParams', '$http',
+  function($rootScope, $scope, $state, $stateParams, $http) {
     $rootScope.currentMenu = 'novo';
     $scope.id_prontuario = $stateParams.id_prontuario;
     $scope.questionario = novoQuestionario();
+
+    $http.get('/prontuarios/' + $stateParams.id_prontuario + '/questionarios').then(function(retorno) {
+      console.log('retorno', retorno);
+      if (retorno && retorno.data && retorno.data.length > 0) {
+        $scope.questionario = retorno.data[0];
+      }
+    });
+
+    $scope.salvar = function() {
+      $http.post('/prontuarios/' + $scope.id_prontuario + '/questionarios', $scope.questionario).then(function() {
+        console.log('Pronto');
+        $state.go('editar_prontuario', {id_prontuario: $scope.id_prontuario});
+      }, function(error) {
+        console.log('Deu erro');
+      });
+    };
 
     $scope.adicionarExameEtiologico = function() {
       $scope.questionario.recemNascido.examesEtiologicos.push($scope.temp.exameEtiologico);
@@ -32,6 +48,29 @@ controllers.controller('NovoQuestionarioController', ['$rootScope', '$scope', '$
       $scope.temp.droga = {};
     };
 
+    $scope.adicionarVacina = function() {
+      $scope.questionario.mae.prenatal.historicoVacinal.push($scope.temp.vacina);
+      $scope.temp.vacina = {
+        numeroDeDoses: 1,
+        dataDoses: []
+      };
+    };
+
+    $scope.adicionarUltrassom = function() {
+      $scope.questionario.mae.prenatal.examesUltrassom.push($scope.temp.ultrassom);
+      $scope.temp.ultrassom = {};
+    };
+
+    $scope.adicionarExame = function() {
+      $scope.questionario.mae.prenatal.exames.push($scope.temp.exame);
+      $scope.temp.exame = {
+        primeiroTrimestre: {},
+        segundoTrimestre: {},
+        terceiroTrimestre: {}
+      };
+    };
+
+
     function novoExameEtiologico() {
       return {
         dados: [
@@ -47,7 +86,17 @@ controllers.controller('NovoQuestionarioController', ['$rootScope', '$scope', '$
         exameEtiologico: novoExameEtiologico(),
         viagem: {},
         medicamento: {},
-        droga: {}
+        droga: {},
+        vacina: {
+          numeroDeDoses: 1,
+          dataDoses: []
+        },
+        ultrassom: {},
+        exame: {
+          primeiroTrimestre: {},
+          segundoTrimestre: {},
+          terceiroTrimestre: {}
+        }
       };
 
       return {
