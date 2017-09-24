e7172f2b266b564c4c7b6dce5ea0da71b3cde74c
Bruno Gama Catao <brunogamacatao@gmail.com>
2017-09-24 17:52:45 -0300

    Trying to automate the deployment process

--- shipitfile.js
+++ shipitfile.js
@@ -15,4 +15,34 @@ module.exports = function (shipit) {
       servers: 'root@45.79.104.38'
     }
   });
+
+  shipit.task('install-dependencies', function () {
+    return shipit.remote('yarn', {
+      cwd: '/root/deploy/current'
+    });
+  });
+
+  shipit.task('build-assets', function () {
+    return shipit.remote('webpack --progress --colors --config webpack.config.js', {
+      cwd: '/root/deploy/current'
+    });
+  });
+
+  shipit.task('stop-server', function () {
+    return shipit.remote('pm2 delete main');
+  });
+
+  shipit.task('start-server', function () {
+    return shipit.remote('pm2 restart ecosystem.config.js --env production', {
+      cwd: '/root/deploy/current'
+    });
+  });
+
+  shipit.on('deploy', function() {
+    shipit.start('stop-server');
+  });
+
+  shipit.on('published', function() {
+    shipit.start(['install-dependencies', 'build-assets', 'start-server']);
+  });
 };
